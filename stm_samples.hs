5dc4cb10b9afa17c5da408c266ed2b46599a3278
John Haynes <john.haynes@mirado.se>
2016-09-22 11:56:34 +0200

    stm sample

---
+++ stm_samples.hs
@@ -0,0 +1,43 @@
+{-
+    compile: ghc -O3 -o ./stm_samples stm_samples.hs -threaded
+    run:     stm_samples +RTS -N
+-}
+
+import Control.Concurrent.STM           (STM, atomically)
+import Control.Concurrent.STM.TVar      (TVar, newTVarIO, writeTVar, readTVar)
+import Control.Concurrent.Async         (async, wait)
+import Control.Monad                    (forM_, forM)
+
+main :: IO ()
+main = do
+
+    -- Initialise some shared, mutable state
+    counter <- newTVarIO 0
+
+    -- Update it inside hundred threads
+    jobs <- forM [1..100] (\_ -> async (doCount counter) )
+
+    -- Wait for the threads to terminate
+    mapM_ wait jobs
+
+    -- Inspect the final value
+    c <- atomically (readTVar counter)
+
+    print c
+
+doCount :: TVar Int -> IO ()
+doCount counter =
+
+    -- Increment a thousand times
+    forM_ [1..1000] $ \_ ->
+
+        {- 'atomically' takes a transaction and runs it,
+            i.e.: it turns an STM() into an IO() -}
+        atomically (atomicIncrement counter)
+
+atomicIncrement :: TVar Int -> STM ()
+atomicIncrement counter = do
+    x <- readTVar counter
+    {- Even though there's a gap here between the read and write,
+       STM makes it "just work" (no race condition!) -}
+    writeTVar counter $! x+1
